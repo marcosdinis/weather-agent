ed571ab337ba5852b8b326b9af0a6ca8457e38b6
Marcos Dinis <itsmarcosdinis@gmail.com>
2026-05-04 09:53:50 -0300

    Refactor weather data extraction and analysis functions for improved readability

--- weather-agent.py
+++ weather-agent.py
@@ -3,10 +3,22 @@ import os
 from dotenv import load_dotenv
 
 
+def temperature_to_celsius(kelvin):
+    return round(kelvin - 273.15, 1)
+
+def temperature_analysis(temperature):
+    if temperature < 18:
+        return "Vista um agasalho, pois a temperatura está baixa."
+    elif 18 <= temperature < 28:
+        return "Vista uma blusa, pois a temperatura está amena."
+    else:
+        return "Vista roupas leves, pois a temperatura está alta."
+    
+    
 def extract_weather_data(data): 
     weather_data = {
-        "temperature": round(data["main"]["temp"] - 273.15, 1),  # Convert from Kelvin to Celsius
-        "wind_chill": round(data["main"]["feels_like"] - 273.15, 1),  # Convert from Kelvin to Celsius
+        "temperature": temperature_to_celsius(data["main"]["temp"]),
+        "wind_chill": temperature_to_celsius(data["main"]["feels_like"]),
         "humidity": round(data["main"]["humidity"], 1),
         "wind_speed": round(data["wind"]["speed"], 1),
         "cloudiness": round(data["clouds"]["all"], 1),
@@ -28,6 +40,14 @@ response = requests.get(url)
 if (response.status_code == 200):
     data = response.json()
     weather_data = extract_weather_data(data)
-    print(weather_data)
+    print("Dados meteorológicos atuais:")
+    print(f"•Temperatura: {weather_data['temperature']}°C")
+    print(f"•Sensação térmica: {weather_data['wind_chill']}°C")
+    print(f"•Umidade: {weather_data['humidity']}%")
+    print(f"•Velocidade do vento: {weather_data['wind_speed']} m/s")
+    print(f"•Nuvens: {weather_data['cloudiness']}%")
+    print(f"•Descrição: {weather_data['description']}")
+    print("\nAnálise de vestimenta:")
+    print(temperature_analysis(weather_data['temperature']))
 else:
     print("Error:", response.status_code)
